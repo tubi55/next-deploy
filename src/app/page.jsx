94848a1323eac363ee44a2aa71016e9a451e25bf
tubi55 <tubi55@nate.com>
2024-01-22 13:04:18 +0900

    test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import styles from './main.module.scss';
 export default function Home() {
 	return (
 		<main className={clsx(styles.main)}>
-			<h1>메인페이지</h1>
+			<h1>메인페이지2</h1>
 		</main>
 	);
 }
